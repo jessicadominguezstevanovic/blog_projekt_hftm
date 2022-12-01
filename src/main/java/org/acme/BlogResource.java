383e736e5d2310b0755878f8fbe5b6c3ea136e85
jessica.dominguez <jessica.dominguez@swissteach.ch>
2022-12-01 11:07:08 +0100

    First commit

---
+++ src/main/java/org/acme/BlogResource.java
@@ -0,0 +1,17 @@
+package org.acme;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/blog")
+public class BlogResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Willkommen im Blog!";
+    }
+    
+}
